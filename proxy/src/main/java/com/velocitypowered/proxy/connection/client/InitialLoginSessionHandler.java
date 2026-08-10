MizuleDEV/Velocity-Sunny

feat: extra events

proxy/src/main/java/com/velocitypowered/proxy/connection/client/InitialLoginSessionHandler.java
@@ -27,6 +27,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.primitives.Longs;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.connection.PreLoginEvent.PreLoginComponentResult;
+import com.velocitypowered.api.event.connection.ValidateSessionEvent;
 import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.crypto.IdentifiedKey;
 import com.velocitypowered.api.util.GameProfile;
@@ -53,6 +54,7 @@ import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.apache.logging.log4j.LogManager;
@@ -224,6 +226,8 @@ public class InitialLoginSessionHandler implements MinecraftSessionHandler {
             }
 
             if (throwable != null) {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, 503, false));
               logger.error("Unable to authenticate player", throwable);
               inbound.disconnect(Component.translatable("multiplayer.disconnect.authservers_down"));
               return;
@@ -253,14 +257,20 @@ public class InitialLoginSessionHandler implements MinecraftSessionHandler {
                       Component.translatable("multiplayer.disconnect.invalid_public_key"));
                 }
               }
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.statusCode(), true));
               // All went well, initialize the session.
               mcConnection.setActiveSessionHandler(StateRegistry.LOGIN,
                   new AuthSessionHandler(server, inbound, profile, true, serverId));
             } else if (response.statusCode() == 204) {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.statusCode(), false));
               // Apparently an offline-mode user logged onto this online-mode proxy.
               inbound.disconnect(
                   Component.translatable("velocity.error.online-mode-only", NamedTextColor.RED));
             } else {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.statusCode(), false));
               // Something else went wrong
               logger.error(
                   "Got an unexpected error code {} whilst contacting Mojang to log in {} ({})",
@@ -268,6 +278,7 @@ public class InitialLoginSessionHandler implements MinecraftSessionHandler {
               inbound.disconnect(Component.translatable("multiplayer.disconnect.authservers_down"));
             }
           }, mcConnection.eventLoop())
+          .orTimeout(15, TimeUnit.SECONDS)
           .thenRun(() -> {
             try {
               httpClient.close();

ssquadteam/ApiaryProxy

(cherry picked from commit e606c474bbd57dfbab8ec02b257432b43e883657)

proxy/src/main/java/com/velocitypowered/proxy/connection/client/InitialLoginSessionHandler.java
@@ -27,6 +27,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.primitives.Longs;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.connection.PreLoginEvent.PreLoginComponentResult;
+import com.velocitypowered.api.event.connection.ValidateSessionEvent;
 import com.velocitypowered.api.network.HandshakeIntent;
 import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.crypto.IdentifiedKey;
@@ -274,6 +275,8 @@ public class InitialLoginSessionHandler implements MinecraftSessionHandler {
             }
 
             if (throwable != null) {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, 503, false));
               LOGGER.error("Unable to authenticate player", throwable);
               inbound.disconnect(Component.translatable("multiplayer.disconnect.authservers_down"));
               return;
@@ -291,14 +294,20 @@ public class InitialLoginSessionHandler implements MinecraftSessionHandler {
                       Component.translatable("multiplayer.disconnect.invalid_public_key"));
                 }
               }
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.getCode(), true));
               // All went well, initialize the session.
               mcConnection.setActiveSessionHandler(StateRegistry.LOGIN,
                   new AuthSessionHandler(server, inbound, profile, true, serverId, appliedResourcePacksFuture));
             } else if (response.getCode() == 204) {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.getCode(), false));
               // Apparently, an offline-mode user logged onto this online-mode proxy.
               inbound.disconnect(
                   Component.translatable("velocity.error.online-mode-only", NamedTextColor.RED));
             } else {
+              server.getEventManager().fireAndForget(
+                  new ValidateSessionEvent(inbound, response.getCode(), false));
               // Something else went wrong
               LOGGER.error(
                   "Got an unexpected error code {} whilst contacting Mojang to log in {} ({})",